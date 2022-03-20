28497917285ae8545f913f6c2d4993c5450fe81b
FabianMatata <mfabian601@gmail.com>
2022-03-20 11:16:24 +0300

    add files

---
+++ neighborhood/urls.py
@@ -0,0 +1,24 @@
+# from django.conf.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+from django.urls import path, include
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('search/', views.search_businesses, name='search_results'),
+    path('business/(\d+)', views.get_business, name='business_results'),
+    path('new/business', views.new_business, name='new-business'),
+    path('new/post', views.new_post, name='new-post'),
+    path('accounts/profile/', views.user_profiles, name='profile'),
+   
+
+    
+
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
